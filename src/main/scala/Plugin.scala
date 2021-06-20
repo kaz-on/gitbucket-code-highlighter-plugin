79e5ba97da2f27802d8b7371192ecf41b5625778
kaz-on <58929100+kaz-on@users.noreply.github.com>
2021-06-20 23:36:18 +0900

    Update description

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -6,7 +6,7 @@ import io.github.gitbucket.solidbase.model.Version
 class Plugin extends gitbucket.core.plugin.Plugin {
   override val pluginId: String = "code-highlighter"
   override val pluginName: String = "Code Highlighter Plugin"
-  override val description: String = "Enhance code syntax highlighting with highlight.js"
+  override val description: String = "Enhance code syntax highlighting using highlight.js"
   override val versions: List[Version] = List(
     new Version("1.0.0"),
     new Version("1.0.1"),
